bea7274056f07b4c99764b571f4d859335d29368
shahoian <ruben.shahoyan@cern.ch>
2021-10-09 15:17:42 +0200

    pass all detector inputs to AODproducer from cmd line

--- Detectors/AOD/src/AODProducerWorkflowSpec.cxx
+++ Detectors/AOD/src/AODProducerWorkflowSpec.cxx
@@ -1315,10 +1315,6 @@ DataProcessorSpec getAODProducerWorkflowSpec(GID::mask_t src, bool useMC)
   dataRequest->requestTracks(src, useMC);
   dataRequest->requestPrimaryVertertices(useMC);
   dataRequest->requestSecondaryVertertices(useMC);
-  dataRequest->requestFT0RecPoints(false);
-  dataRequest->requestFV0RecPoints(false);
-  dataRequest->requestFDDRecPoints(false);
-  dataRequest->requestClusters(GIndex::getSourcesMask("TPC,TOF"), false);
 
   outputs.emplace_back(OutputLabel{"O2bc"}, "AOD", "BC", 0, Lifetime::Timeframe);
   outputs.emplace_back(OutputLabel{"O2cascade"}, "AOD", "CASCADE", 0, Lifetime::Timeframe);
