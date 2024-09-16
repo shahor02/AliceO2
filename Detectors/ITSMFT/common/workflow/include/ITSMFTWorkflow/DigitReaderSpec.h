5fde5ce1fc72a35e1113caabfbb176c1b4827433
shahoian <ruben.shahoyan@cern.ch>
2024-09-16 23:45:13 +0400

    Adapth ITS/MFT digit reader to IRFrames filtering

--- Detectors/ITSMFT/common/workflow/include/ITSMFTWorkflow/DigitReaderSpec.h
+++ Detectors/ITSMFT/common/workflow/include/ITSMFTWorkflow/DigitReaderSpec.h
@@ -24,6 +24,7 @@
 #include "Headers/DataHeader.h"
 #include "DataFormatsITSMFT/ROFRecord.h"
 #include "DetectorsCommonDataFormats/DetID.h"
+#include "SimulationDataFormat/IOMCTruthContainerView.h"
 
 using namespace o2::framework;
 
@@ -48,7 +49,6 @@ class DigitReader : public Task
   std::vector<o2::itsmft::GBTCalibData> mCalib, *mCalibPtr = &mCalib;
   std::vector<o2::itsmft::ROFRecord> mDigROFRec, *mDigROFRecPtr = &mDigROFRec;
   std::vector<o2::itsmft::MC2ROFRecord> mDigMC2ROFs, *mDigMC2ROFsPtr = &mDigMC2ROFs;
-
   o2::header::DataOrigin mOrigin = o2::header::gDataOriginInvalid;
 
   std::unique_ptr<TFile> mFile;
@@ -56,6 +56,9 @@ class DigitReader : public Task
   bool mUseMC = true;    // use MC truth
   bool mUseCalib = true; // send calib data
   bool mTriggerOut = true; // send dummy triggers vector
+  bool mUseIRFrames = false; // selected IRFrames modes
+  int mROFBiasInBC = 0;
+  int mROFLengthInBC = 0;
   std::string mDetName = "";
   std::string mDetNameLC = "";
   std::string mFileName = "";
