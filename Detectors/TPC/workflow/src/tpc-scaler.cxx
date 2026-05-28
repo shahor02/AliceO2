51140d1416b095e6d6f205c7dfbf90b0520c7495
shahoian <ruben.shahoyan@cern.ch>
2026-05-28 18:17:47 +0200

    TPC scaler can force reading CTP Lumi

--- Detectors/TPC/workflow/src/tpc-scaler.cxx
+++ Detectors/TPC/workflow/src/tpc-scaler.cxx
@@ -16,8 +16,16 @@
 #include "CommonUtils/ConfigurableParam.h"
 #include "Framework/ConfigParamSpec.h"
 #include "TPCCalibration/CorrectionMapsOptions.h"
+#include "GlobalTrackingWorkflowHelpers/InputHelper.h"
+#include "DetectorsRaw/HBFUtilsInitializer.h"
+#include "Framework/CallbacksPolicy.h"
 
 using namespace o2::framework;
+
+void customize(std::vector<o2::framework::CallbacksPolicy>& policies)
+{
+  o2::raw::HBFUtilsInitializer::addNewTimeSliceCallback(policies);
+}
 
 void customize(std::vector<o2::framework::ConfigParamSpec>& workflowOptions)
 {
@@ -25,7 +33,9 @@ void customize(std::vector<o2::framework::ConfigParamSpec>& workflowOptions)
   std::vector<ConfigParamSpec> options{
     ConfigParamSpec{"configKeyValues", VariantType::String, "", {"Semicolon separated key=value strings"}},
     {"enable-M-shape-correction", VariantType::Bool, false, {"Enable M-shape distortion correction"}},
+    {"disable-root-input", VariantType::Bool, false, {"disable root-files input reader (e.g. for CTP input)"}},
     {"disable-IDC-scalers", VariantType::Bool, false, {"Disable TPC scalers for space-charge distortion fluctuation correction"}}};
+  o2::raw::HBFUtilsInitializer::addConfigOption(options);
   o2::tpc::CorrectionMapsOptions::addGlobalOptions(options);
   std::swap(workflowOptions, options);
 }
@@ -39,6 +49,12 @@ WorkflowSpec defineDataProcessing(ConfigContext const& config)
   const auto enableMShape = config.options().get<bool>("enable-M-shape-correction");
   const auto enableIDCs = !config.options().get<bool>("disable-IDC-scalers");
   auto sclOpt = o2::tpc::CorrectionMapsOptions::parseGlobalOptions(config.options());
+  if (sclOpt.requestCTPLumi) {
+    o2::dataformats::GlobalTrackID::mask_t dummy, src = o2::dataformats::GlobalTrackID::getSourcesMask("CTP");
+    o2::globaltracking::InputHelper::addInputSpecs(config, workflow, src, src, src, false, dummy); // clusters MC is not needed
+  }
   workflow.emplace_back(o2::tpc::getTPCScalerSpec(enableIDCs, enableMShape, sclOpt));
+  // configure dpl timer to inject correct firstTForbit: start from the 1st orbit of TF containing 1st sampled orbit
+  o2::raw::HBFUtilsInitializer hbfIni(config, workflow);
   return workflow;
 }
